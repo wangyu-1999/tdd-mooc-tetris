1910afcd7c74b0ab4d1e9ca7caaf47f386e25c78
王昱 <wangyu2021@foxmail.com>
2025-04-25 17:56:20 +0800

    feat: implement empty board, pass the empty board test

--- src/Board.mjs
+++ src/Board.mjs
@@ -5,9 +5,10 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = new Array(height).fill(new Array(width).fill("."));
   }
 
   toString() {
-    return "TODO";
+    return this.board.map((r) => r.join("")).join("\n") + "\n";
   }
 }
