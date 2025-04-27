399295d4730b066988ada1ff9b8448e59deecfd9
王昱 <wangyu2021@foxmail.com>
2025-04-27 15:35:53 +0800

    pass the 5*5 rotate test

--- test/RotatingShapes.test.mjs
+++ test/RotatingShapes.test.mjs
@@ -34,7 +34,6 @@ describe("Rotating 3x3 shape", () => {
   });
 });
 
-/*
 describe("Rotating 5x5 shape", () => {
   const shape = RotatingShape.fromString(
     `ABCDE
@@ -54,7 +53,7 @@ describe("Rotating 5x5 shape", () => {
     );
   });
 
-  test.skip("can be rotated right/clockwise", () => {
+  test("can be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
       `UPKFA
        VQLGB
@@ -64,7 +63,7 @@ describe("Rotating 5x5 shape", () => {
     );
   });
 
-  test.skip("can be rotated left/counter-clockwise", () => {
+  test("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `EJOTY
        DINSX
@@ -74,4 +73,3 @@ describe("Rotating 5x5 shape", () => {
     );
   });
 });
-*/
