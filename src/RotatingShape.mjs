2ac99cfe87c08abce591286da218768c2b6fb0e3
王昱 <wangyu2021@foxmail.com>
2025-04-27 11:23:15 +0800

    feat: implement rotate

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -3,4 +3,16 @@ export class RotatingShape {
   constructor(shape) {
     this.shape = shape;
   }
+  rotate() {
+    const size = this.shape.length;
+    const rotatedShape = Array.from({ length: size }, () => Array(size).fill(null));
+
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        rotatedShape[j][size - 1 - i] = this.shape[i][j];
+      }
+    }
+
+    return new RotatingShape(rotatedShape);
+  }
 }
